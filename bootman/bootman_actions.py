4c357820b92bcb940330116bb3394cae1b70dbf5
Bardia Moshiri <bardia@furilabs.com>
2025-01-09 20:40:52 -0500

    bootman_actions: drop -p from e2fsck and -t from mke2fs
    ext4 is default and -t is not an option in busybox
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- bootman/bootman_actions.py
+++ bootman/bootman_actions.py
@@ -84,12 +84,12 @@ def create_install_commands(name, size):
 
     # Write the command file
     commands = [
-        "e2fsck -p -y -f /dev/droidian/droidian-rootfs",
+        "e2fsck -fy /dev/droidian/droidian-rootfs",
         f"resize2fs /dev/droidian/droidian-rootfs {new_size_mb}M",
         f"lvm lvreduce -L -{size_mb}M -r /dev/droidian/droidian-rootfs",
         f"lvm lvcreate -L {size_mb}M -n {partition_name} droidian",
-        f"mke2fs -t ext4 /dev/droidian/{partition_name}",
-        f"e2fsck -p -y -f /dev/droidian/{partition_name}"
+        f"mke2fs /dev/droidian/{partition_name}",
+        f"e2fsck -fy /dev/droidian/{partition_name}"
     ]
     success, _ = run_helper("write_commands", "\n".join(commands))
     if not success:
